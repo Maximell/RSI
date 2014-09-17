c79ce31898d933d1a1bff9a1f3de5873a02847a8
Maxim Ellison <ellisonmaxim@gmail.com>
2014-09-17 16:35:25 -0700

    added createPrompt

--- sample.c
+++ sample.c
@@ -4,24 +4,38 @@
 #include <unistd.h>
 #include <readline/readline.h>
 #include <readline/history.h>
+//#include <linux/limits.h> for lab machines - defines MAX_PATH
+#include <sys/syslimits.h> // for osx - TODO remove before handing in
+
+
+
+char* createPrompt(const char* start, char* middle, const char* end, char* prompt){ //takes the 3 parts of the prompt, and puts them together.
+
+	strcpy(prompt, start); //copy the start into the prompt
+	strcat(prompt, middle); //add on the middle
+	strcat(prompt, end); //add on the end
+
+	return prompt; //return the prompt
+}
 
 int main() {
+	const char* prePrompt = "RSI: ";
+	const char* postPrompt = " > ";
+	char prompt[PATH_MAX+6+4];
+	char currentDir[PATH_MAX]; //allocate the maximum possible length of path, from linux/limits.h
 
-	char* currentDir; //need to change to dynamically allocated
-
-	if (getwd(currentDir) != NULL){ //attempts to get the current working directory
+	if (getcwd(currentDir, sizeof(currentDir)) != NULL){ //attempts to get the current working directory
 		printf("%s\n\n", currentDir); // if successful, 
-		printf("blah");
 	} else {
 		perror("getcwd() error");
 		return -1;
 	}
-	printf("blah");
+
+	createPrompt(prePrompt, currentDir, postPrompt, prompt);
 
 	int bailout = 0;
 	while (!bailout) {
-		printf("blah");
-		char* reply = readline(currentDir);
+		char* reply = readline(prompt);
 		/* Note that readline strips away the final \n */
 		/* For Perl junkies, readline automatically chomps the line read */
 
